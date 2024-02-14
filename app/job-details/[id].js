6ae4fd7db1cbeed68501be1861c3e40b3e03a6ec
ergunDevv <ergncyln2127088@gmail.com>
2024-02-14 11:10:10 +0300

    From api company's data passed to components

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -67,7 +67,12 @@ const JobDetails = () => {
             <Text>There is no data!</Text>
           ) : (
             <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
-              <Company />
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].company_name}
+                location={data[0].job_country}
+              />
               <JobTabs />
             </View>
           )}
