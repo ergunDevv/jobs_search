24d333258d0516ec498e3a34febbbb3dd9147c22
ergunDevv <ergncyln2127088@gmail.com>
2024-02-20 11:10:10 +0300

    JobAbout component added for about tab

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -40,6 +40,11 @@ const JobDetails = () => {
           />
         );
       case "About":
+        return (
+          <JobAbout
+            info={data[0].job_description ?? "No data provided about the job."}
+          />
+        );
       case "Responsibilities":
 
       default:
