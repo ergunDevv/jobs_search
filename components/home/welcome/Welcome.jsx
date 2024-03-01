e9cb1c288586fe444996b318aea88df1d34800ae
ergunDevv <ergncyln2127088@gmail.com>
2024-03-01 09:10:10 +0300

    Search functionality error solved(Typo)

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -37,7 +37,7 @@ const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
         <View style={styles.searchWrapper}>
           <TextInput
             style={styles.searchInput}
-            value={searchTermm}
+            value={searchTerm}
             onChange={(text) => {
               setSearchTerm(text);
             }}
