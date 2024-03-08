1f74601fc193683c8f2d5f57d922428e4d64c90a
ergunDevv <ergncyln2127088@gmail.com>
2024-03-08 14:35:57 +0300

    initial project

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import { useState } from "react";
+import { Stack, useRouter } from "react-router-native";
 
-export default function App() {
+const Home = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Home Fahri Naber</Text>
+      <Text>Home Fahri Naber</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Home;
